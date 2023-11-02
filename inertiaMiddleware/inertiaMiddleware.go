2fa1e933cae0e52754fa8140b2e02d78b6d6d763
kevinchar93 <kevincharles018@gmail.com>
2023-11-02 13:31:09 +0000

    create InertiaInfo struct & function RegisterInertiaAdapter

--- inertiaMiddleware/inertiaMiddleware.go
+++ inertiaMiddleware/inertiaMiddleware.go
@@ -1,6 +1,26 @@
 package inertiaMiddleware
 
-import "github.com/labstack/echo/v4"
+import (
+	"log"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+type InertiaInfo struct {
+	pageTemplate *template.Template
+}
+
+func RegisterInertiaAdapter(echoInstance *echo.Echo) {
+	var inertiaInfo *InertiaInfo = new(InertiaInfo)
+
+	template, err := template.New("app.html").ParseFiles("app.html")
+	if err != nil {
+		log.Fatal("Failed to load the template", err)
+	}
+
+	inertiaInfo.pageTemplate = template
+}
 
 func InertiaMiddleware(e *echo.Echo) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
