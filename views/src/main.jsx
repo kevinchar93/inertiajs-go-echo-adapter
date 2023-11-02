9f7d42fd50faf1b80116cfeae7148f5dc020a96a
kevinchar93 <kevincharles018@gmail.com>
2023-11-02 12:39:02 +0000

    make modification to vite template to load InertiaJS

--- views/src/main.jsx
+++ views/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './main.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./pages/**/*.jsx', { eager: true })
+    return pages[`./pages/${name}.jsx`]
+  },
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />)
+  },
+})
