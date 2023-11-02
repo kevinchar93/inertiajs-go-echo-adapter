ce6117059f6129fe503280622db6ef63fa43035f
kevinchar93 <kevincharles018@gmail.com>
2023-11-02 18:10:37 +0000

    update index page to render list of pokemon

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
+	"cmp"
 	"inertia-echo/page"
 	"inertia-echo/pokemondb"
 	"net/http"
+
+	"slices"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,9 +16,23 @@ func ConfigureRoutes(e *echo.Echo, db map[int]pokemondb.PokemonInfo) {
 
 		header := page.NewPageHeader("Index title", "meta name", "meta content")
 
+		type PokemonListItem struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+		}
+		var allPokemon []PokemonListItem
+
+		for _, pokemon := range db {
+			allPokemon = append(allPokemon, PokemonListItem{ID: pokemon.ID, Name: pokemon.Name.English})
+		}
+
+		slices.SortFunc(allPokemon, func(a, b PokemonListItem) int {
+			return cmp.Compare(a.ID, b.ID)
+		})
+
 		props := map[string]interface{}{
-			"exampleProp": "Let's a go!",
-			"header":      header,
+			"header":     header,
+			"allPokemon": allPokemon,
 		}
 
 		return ctx.Render(http.StatusOK, "Index", props)
--- views/src/pages/Index.jsx
+++ views/src/pages/Index.jsx
@@ -4,42 +4,22 @@ import viteLogo from '/vite.svg'
 import './Index.css'
 import { Link, Head } from "@inertiajs/react";
 
-function Index({ header, exampleProp }) {
-  const [count, setCount] = useState(0)
-
+function Index({ header, allPokemon }) {
   const { title, meta } = header
 
   return (
-    <>
+    <div>
       <Head>
         <title>{title}</title>
         <meta name={meta.name} content={meta.content} />
       </Head>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>
-        <Link href='/example-page'>Link to example page</Link>
-      </h2>
-      <h2>{exampleProp}</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>Simple Pokedex</h1>
+      <ul>
+        {allPokemon.map(({ id, name }) =>
+          (<li key={id}>{id} - {name}</li>)
+        )}
+      </ul>
+    </div>
   )
 }
 
